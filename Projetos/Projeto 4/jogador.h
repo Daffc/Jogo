038c9785a54dd3e513f8206f035f52c024a5e199
Douglas AC <dac17@inf.ufpr.br>
2017-10-22 04:04:24 -0200

    Biblioteca para criação e gerenciamento de jogador.

---
+++ Projetos/Projeto 4/jogador.h
@@ -0,0 +1,22 @@
+#ifndef _JOGADOR_
+#define _JOGADOR_
+
+typedef struct Jogador
+{
+    int 		    xPosi, 
+                    yPosi,
+                    xInic,
+                    yInic;
+
+    char		    frentecima,
+                    frentebaixo,
+                    trascima ,
+                    trasbaixo ;
+} Jogador;
+
+void remove_jogador (Jogador *jogador, WINDOW *tela);
+void reposiciona_jogador(Jogador *jogador, WINDOW *tela);
+void iniciar_jogador(Jogador *jogador, WINDOW *tela);
+int capi_movimento(Jogador *jogador, WINDOW *tela, int max_larg, int max_comp);
+
+#endif
